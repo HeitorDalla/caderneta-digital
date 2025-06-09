037df1c4a636684825c1790e808f5ea9a381c851
Matheus Coronado <matheuscoronado48@gmail.com>
2025-06-09 19:40:09 -0300

    Adicionado arquivo responsavél pela ligação do banco de dados com o frontend

---
+++ backend/models/database.php
@@ -0,0 +1,20 @@
+<?php
+class Database {
+    private static $instance = null;
+
+    public static function getConnection() {
+        if (!self::$instance) {
+            $host = 'localhost';
+            $port = '3307'; 
+            $db = 'cadernetadigital';
+            $user = 'root';
+            $password = '';
+
+            
+            self::$instance = new PDO("mysql:host=$host;port=$port;dbname=$db", $user, $password);
+            self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+        return self::$instance;
+    }
+}
+?>
