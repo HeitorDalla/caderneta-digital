70874688b2aadd513e2aa79bc75d41155487ab13
HeitorDalla <heitorvillavilla@gmail.com>
2025-07-01 21:57:01 -0300

    Modelo banco de dados

---
+++ database/modeloInicial2.sql
@@ -0,0 +1,36 @@
+create database cadernetaDigital;
+
+use cadernetaDigital;
+
+-- Usuários (Alunos, Professores, Admins)
+CREATE TABLE Usuario (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100),
+    email VARCHAR(100) UNIQUE,
+    senha_hash VARCHAR(255),
+    tipo ENUM('aluno', 'professor', 'administrador'),
+    ativo BOOLEAN DEFAULT TRUE,
+    criado_em DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Atividades Práticas
+CREATE TABLE Atividade (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    titulo VARCHAR(100),
+    subtitulo VARCHAR(100),
+    descricao TEXT,
+    data DATE,
+    aluno_id INT,
+    FOREIGN KEY (aluno_id) REFERENCES Usuario(id)
+);
+
+-- Feedback
+CREATE TABLE Feedback (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    professor_id INT,
+    atividade_id INT,
+    comentario TEXT,
+    data_feedback DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (professor_id) REFERENCES Usuario(id),
+    FOREIGN KEY (atividade_id) REFERENCES Atividade(id)
+);
